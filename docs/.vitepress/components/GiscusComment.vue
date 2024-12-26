4937e215929e803c9ca66053987b320bea8f1078
miksin <contact@miksin.art>
2024-12-26 21:06:33 +0800

    giscus

---
+++ docs/.vitepress/components/GiscusComment.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import Giscus from '@giscus/vue'
+import { useData, useRoute } from 'vitepress'
+
+const route = useRoute()
+const { isDark } = useData()
+</script>
+
+<template>
+  <div :class="$style.giscus">
+    <Giscus
+      :key="route.path"
+      repo="miksin/miksinote"
+      repo-id="R_kgDONgPjug"
+      category="General"
+      category-id="DIC_kwDONgPjus4Clh1d"
+      mapping="pathname"
+      term="Welcome to giscus"
+      strict="0"
+      reactions-enabled="1"
+      emit-metadata="0"
+      input-position="top"
+      :theme="isDark ? 'dark' : 'light'"
+      lang="zh-TW"
+      loading="lazy"
+    />
+  </div>
+</template>
+
+<style lang="css" module>
+.giscus {
+  margin-top: 2rem;
+}
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme-without-fonts'
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
+import GiscusComment from '../components/GiscusComment.vue'
 import './style.css'
 
 export default {
@@ -9,6 +10,7 @@ export default {
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'doc-after': () => h(GiscusComment),
     })
   },
   enhanceApp({ app: _app, router: _router, siteData: _siteData }) {
