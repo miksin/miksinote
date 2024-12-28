61e744485cb32a1da2f3a636a28eabc509542bda
miksin <contact@miksin.art>
2024-12-28 09:52:13 +0800

    layout

--- docs/.vitepress/components/NotesPreview.vue
+++ docs/.vitepress/components/NotesPreview.vue
@@ -6,12 +6,13 @@ import { data as posts } from '../theme/posts.data.mjs'
   <h3>Recents</h3>
   <div :class="$style.container">
     <div v-for="post in posts" :key="post.url" :class="$style.block">
-      <article :class="$style.article">
-        <a :href="post.url">
-          {{ post.title }}
-        </a>
-        <div v-html="post.excerpt" />
-      </article>
+      <a :href="post.url">
+        {{ post.title }}
+      </a>
+      <div v-html="post.excerpt" />
+      <div :class="$style.timestamp">
+        {{ post.date.string }}
+      </div>
     </div>
   </div>
 </template>
@@ -21,18 +22,25 @@ import { data as posts } from '../theme/posts.data.mjs'
   margin-top: 1rem;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-auto-rows: auto;
   gap: 1rem 0.75rem;
 }
 
 .block {
+  display: grid;
+  grid-row: span 3;
+  grid-template-rows: subgrid;
+  row-gap: 0;
+
   padding: 1.5rem;
   border: 1px solid var(--vp-c-bg-soft);
   background-color: var(--vp-c-bg-soft);
   border-radius: 0.75rem;
 }
 
-.article {
-  display: flex;
-  flex-direction: column;
+.timestamp {
+  margin-left: auto;
+  font-size: var(--vp-font-size-small);
+  color: var(--vp-c-text-soft);
 }
 </style>
